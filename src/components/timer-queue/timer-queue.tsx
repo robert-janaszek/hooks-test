1765667583f431bd59974d736efbdbafd30df00e
Robert <robert.janaszek@gmail.com>
2022-04-22 20:56:55 +0200

    Refine timer queue component

--- src/components/timer-queue/timer-queue.tsx
+++ src/components/timer-queue/timer-queue.tsx
@@ -1,7 +1,14 @@
 import { useWindowWorkerQueue } from "../../services/window-worker-queue-hook";
 
 export const TimerAndQueue = () => {
-  useWindowWorkerQueue()
+  useWindowWorkerQueue();
 
-  return null;
+  return <>
+    <div>
+      Timer and Queue component
+    </div>
+    <div className='open-console'>
+      Open console. Execution is similar to Queue, but it halts when window is hidden.
+    </div>
+  </>;
 }
