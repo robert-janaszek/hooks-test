95ea54704c8751fda2449825749ca32434300041
Robert <robert.janaszek@gmail.com>
2022-04-22 20:55:11 +0200

    Refine queue component

--- src/components/queues/queues.tsx
+++ src/components/queues/queues.tsx
@@ -4,5 +4,12 @@ import { useAppQueue } from "../../services/app-queue-hook"
 export const Queues = () => {
   useAppQueue();
 
-  return null;
+  return <>
+    <div>
+      Queues component
+    </div>
+    <div className='open-console'>
+      Open console and observe heavy tasks being processed in queue (by two).
+    </div>
+  </>;
 }
