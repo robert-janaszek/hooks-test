5f6a6046f586b58261172bb2b61496914d66e177
Robert <robert.janaszek@gmail.com>
2022-04-22 20:57:11 +0200

    Use circular state to rotate between pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,18 @@ import './App.css'
 import { Queues } from './components/queues/queues'
 import { TimerAndQueue } from './components/timer-queue/timer-queue'
 import { TimerComponent } from './components/timer/timer'
+import { useCircularState } from './services/circular-state-hook'
 
 function App() {
+  const [page, nextPage] = useCircularState([1, 2, 3])
   return (
     <div className="App">
-      <TimerComponent />
-      <Queues />
-      <TimerAndQueue />
+      <div>
+        <button onClick={() => nextPage()}>next</button>
+      </div>
+      {page === 1 && <TimerComponent />}
+      {page === 2 && <Queues />}
+      {page === 3 && <TimerAndQueue />}
     </div>
   )
 }
