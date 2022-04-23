84a45cec6bb28aa939bfa53c7478a70dd583c5af
Robert <robert.janaszek@gmail.com>
2022-04-23 16:46:29 +0200

    Add failing entries

--- src/services/worker/worker.ts
+++ src/services/worker/worker.ts
@@ -1,7 +1,12 @@
 const heavyEntries = ['II', 'IV', 'V', 'VI'];
+const failingEntries = ['XI'];
 
 export const worker = (entry: string) => {
-  return new Promise<string>((resolve) => {
+  return new Promise<string>((resolve, reject) => {
+    if (failingEntries.includes(entry)) {
+      window.setTimeout(() => reject(new Error('There was na error')), 500);
+      return;
+    }
     if (heavyEntries.includes(entry)) {
       window.setTimeout(() => resolve('Result ' + entry), 6500);
       return;
